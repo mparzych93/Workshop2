c4ac9ccc6accc1f6e92dc4273a32b8151cfc3675
mparzych93 <mateuszparzych.93@gmail.com>
2023-06-06 21:58:45 +0200

    second commit

--- src/main/java/pl/coderslab/entity/MainDao.java
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -8,25 +8,25 @@ public class MainDao {
         UserDao userDao = new UserDao();
 
         //creating user
-/*        User userToAdd = new User();
+        User userToAdd = new User();
         userToAdd.setUsername("mateusz");
         userToAdd.setEmail("mateusz@gmail.com");
         userToAdd.setPassword("slabe");
-        userDao.create(userToAdd);*/
+        userDao.create(userToAdd);
 
         //reading user
-/*        User userToRead = userDao.read(2);
-        System.out.println(userToRead);*/
+        User userToRead = userDao.read(2);
+        System.out.println(userToRead);
 
         //updating user
-/*        User userToUpdate = userDao.read(1);
+        User userToUpdate = userDao.read(1);
         userToUpdate.setUsername("Arkadiusz");
         userToUpdate.setEmail("arek@coderslab.pl");
         userToUpdate.setPassword("superPassword");
-        userDao.update(userToUpdate);*/
+        userDao.update(userToUpdate);
 
         //deleting user
-/*        userDao.delete(1);*/
+        userDao.delete(1);
 
         //finding all users
         User[] arrOfUsers = userDao.findAll();
